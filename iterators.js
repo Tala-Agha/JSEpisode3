dd0093a6230aadeb32ebd9d443a290d6c56c58a2
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-03-08 12:33:47 +0200

    JS task3

--- iterators.js
+++ iterators.js
@@ -5,7 +5,7 @@
  * - Logs every element of the array
  ************************************/
 const logger = function(array) {
-  // Your code here
+  array.forEach(food => console.log(food));
 };
 
 /*************************************
@@ -20,7 +20,8 @@ const logger = function(array) {
  *   C = (F - 32) * (5/9)
  ************************************/
 const toCelsius = function(temperatures) {
-  // Your code here
+  return temperatures.map(temperature => (temperature - 32) * (5/9));
+  
 };
 
 /**************************************
@@ -32,7 +33,7 @@ const toCelsius = function(temperatures) {
  *   that exceed the threshhold
  ***************************************/
 const hottestDays = function(temperatures, threshhold) {
-  // Your code here
+  return threshhold = temperatures.filter(temperature => temperature > threshhold)
 };
 
 /******************************************
@@ -50,7 +51,7 @@ const hottestDays = function(temperatures, threshhold) {
  *       all previous functions
  *******************************************/
 const logHottestDays = function(temperatures, threshhold) {
-  // Your code here
+  logger(toCelsius(hottestDays(temperatures,threshhold)))
 };
 
 export { logger, toCelsius, hottestDays, logHottestDays };
